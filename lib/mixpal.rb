3e89429b3a2c0070d0700d29e9cde2a40a2e2f40
Michael Morrissey <mikehmorrissey@gmail.com>
2015-05-04 13:57:08 -0400

    add custom events to mixpal

--- lib/mixpal.rb
+++ lib/mixpal.rb
@@ -1,4 +1,5 @@
 require 'mixpal/version'
+require 'active_support'
 require 'active_support/core_ext'
 
 module Mixpal
@@ -7,4 +8,17 @@ module Mixpal
   autoload :Event, 'mixpal/event'
   autoload :User, 'mixpal/user'
   autoload :Integration, 'mixpal/integration'
+
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :helper_module
+  end
 end
--- lib/mixpal/tracker.rb
+++ lib/mixpal/tracker.rb
@@ -2,6 +2,10 @@ module Mixpal
   class Tracker
     attr_reader :events, :user_updates, :identity, :alias_user
 
+    if Mixpal.configuration && Mixpal.configuration.helper_module
+      include Mixpal.configuration.helper_module
+    end
+
     STORAGE_KEY = 'mixpal'
 
     def initialize(args = {})
