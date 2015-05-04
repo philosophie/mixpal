f820fb1edd3625adb1019d5f9d79a9024213b8e0
Michael Morrissey <mikehmorrissey@gmail.com>
2015-05-04 13:57:08 -0400

    add custom events to mixpal

--- lib/mixpal.rb
+++ lib/mixpal.rb
@@ -7,4 +7,17 @@ module Mixpal
   autoload :Event, 'mixpal/event'
   autoload :User, 'mixpal/user'
   autoload :Integration, 'mixpal/integration'
+
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :helper_module
+  end
 end
